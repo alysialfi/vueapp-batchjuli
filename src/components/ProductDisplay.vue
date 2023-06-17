5670452dd9311f7b0f55d64d5bb6bd7f44a4c8cf
alfi <alfi@domainesia.com>
2023-06-17 17:32:49 +0700

    initial commit

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,56 @@
+<script>
+    export default {
+        name: 'ProductDisplay',
+        data() {
+            return {
+                price: 75,
+                amount: 0,
+                // subTotal: 0,
+                isAmountGreaterThan3: false
+            }
+        },
+        methods: {
+            addToCart() {
+                if (this.amount > 3) {
+                    this.isAmountGreaterThan3 = true
+                }
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="flex w-full">
+        <div class="w-1/2 h-screen bg-gray-200">
+            <!-- thumbnail -->
+            <img src="../assets/images/sofa.png" alt="sofa thumbnail">
+        </div>
+        <div class="w-1/2">
+            <!-- details -->
+            <div>
+                <!-- title & desc -->
+                <h1>Greyana Sofa</h1>
+                <p>
+                    Introducing the Greyana Sofa. The perfect combination of style and comfort. With its sleek and modern design, this sofa is a statement piece that will elevate any living space. The Greyana Sofa features a stunning grey upholstery that is both soft to the touch and durable for everyday use. Crafted with a sturdy wooden frame and high-density foam cushions, the Greyana Sofa offers unbeatable comfort and support. The seat and back cushions are also removable and reversible, making it easy to maintain and keep looking like new.
+                </p>
+            </div>
+            <div>
+                <!-- price, counter, & bubtton -->
+                <div class="text-3xl font-semibold">
+                    ${{ price }}
+                </div>
+                <div class="flex mt-2">
+                    <button @click="amount--" class="bg-black text-white px-2">-</button>
+                    <div class="border border-black px-5 bg-gray-100">{{ amount }}</div>
+                    <button @click="amount++" class="bg-black text-white  px-2">+</button>
+                </div>
+                <p v-if="isAmountGreaterThan3">maximum items that can be added is 3</p>
+                <div class="text-xl font-semibold mt-10">Subtotal: ${{ price * amount }}</div>
+                <div class="flex w-full justify-between items-center gap-5 mt-2 mr-5">
+                    <button class="text-center w-full bg-black text-white py-4">Buy Now</button>
+                    <button @click="addToCart()" class="text-center w-full bg-black text-white py-4">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
